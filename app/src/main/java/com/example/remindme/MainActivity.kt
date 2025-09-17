db5431f44fb24e00195429a76e89a4ad138e755d
Max Ang <maxang12@gmail.com>
2025-09-17 17:06:21 +0800

    Edit ResponsiveContainer

--- app/src/main/java/com/example/remindme/MainActivity.kt
+++ app/src/main/java/com/example/remindme/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.remindme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+import com.example.remindme.ui.view.OverviewScreen
+import com.example.remindme.ui.view.TaskListScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,11 +19,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val navController = rememberNavController()
-            ResponsiveContainer{
-                RemindMeApp(navController)
-            }
 
+            // can run here due to context but not in the ResponsiveContainer Screen.
+            // dynamic collectAsState
+            // updates every recomposition
+            val windowInfo by WindowInfoTracker.getOrCreate(this)
+                .windowLayoutInfo(this)
+                .collectAsState(initial = null)
+            if (windowInfo == null) {
+                // render nothing
+            } else {
+                val foldingFeature =
+                    windowInfo?.displayFeatures?.find { it is FoldingFeature } as? FoldingFeature
+
+                val navController = rememberNavController()
+                ResponsiveContainer(
+                    content = {RemindMeApp(navController)},
+                    extraContents = listOf(
+                        { OverviewScreen(
+                            onGoToList = {navController.navigate("list")},
+                            onGoToMain = {navController.navigate("main")}
+                        ) },
+                        {TaskListScreen()}
+                    ),
+                    foldingFeature = foldingFeature
+                )
+            }
 
         }
     }
--- app/src/main/java/com/example/remindme/ResponsiveContainer.kt
+++ app/src/main/java/com/example/remindme/ResponsiveContainer.kt
@@ -1,31 +1,158 @@
 package com.example.remindme
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.window.core.layout.WindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.window.core.layout.WindowWidthSizeClass
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
+
 import androidx.compose.ui.platform.LocalContext
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun ResponsiveContainer(
     modifier: Modifier = Modifier,
     windowSizeClass: WindowSizeClass = currentWindowAdaptiveInfo().windowSizeClass,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
+    extraContents: List<@Composable () -> Unit> = emptyList(),
+    foldingFeature: FoldingFeature? = null
 ) {
+    // Checking screen size
     println("Window Size Class Details of current screen")
     println(windowSizeClass)
-    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    // Checking folding feature
+    println("Current folding feature is:")
+    println(foldingFeature)
+
+//    val context = LocalContext.current
+//    val windowInfo by WindowInfoTracker.getOrCreate(context)
+//        .windowLayoutInfo(context as ComponentActivity)
+//        .collectAsState(initial = null)
+    // Observe WindowLayoutInfo as state
+//    val windowInfo by produceState(initialValue = null) {
+//        val tracker = WindowInfoTracker.getOrCreate(context)
+//        tracker.windowLayoutInfo(context as ComponentActivity)
+//            .collect { value = it }
+//    }
+//    val foldingFeature = windowInfo?.displayFeatures
+//        ?.filterIsInstance<FoldingFeature>()
+//        ?.firstOrNull()
+//    println(foldingFeature)
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+    }
 
     // TODO:
     // 1. Different Screen Sizes
     // Based on WindowSizeClass width and height values --> design panes for the Composables (1 or 2)
     // 2. If foldable device, then:
     // Use WindowInfoTracker to expose window layout information (Kotlin Flow)
-    Box(modifier = modifier.fillMaxSize()) {
-        content()
+
+    if (foldingFeature == null) {
+        println("Not a foldable device")
+        when (windowSizeClass.windowWidthSizeClass) {
+            WindowWidthSizeClass.COMPACT,
+            WindowWidthSizeClass.MEDIUM -> {
+                // Show a single pane
+                println("Showing single pane")
+                Box(modifier = modifier.fillMaxSize()) {
+                    content()
+                }
+            }
+
+            // For EXPANDED and larger widths
+            else -> {
+                // Show multiple panes side by side, going through the list of composables
+                // if the list is empty, just show a single pane for content composable
+                val allContents = listOf(content) + extraContents
+                println("ELSE: Showing multiple panes")
+                Row(modifier = modifier.fillMaxSize()) {
+                    allContents.forEach {
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxHeight()
+                        ) {
+                            it()
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        when (foldingFeature.state) {
+            // Opened up halfway, like a book -> show up to 2 panes max
+            FoldingFeature.State.HALF_OPENED -> {
+                when (foldingFeature.orientation) {
+                    // TABLETOP MODE
+                    FoldingFeature.Orientation.HORIZONTAL -> {
+                        // Show a single pane
+                        println("Showing single pane")
+                        Box(modifier = modifier.fillMaxSize()) {
+                            content()
+                        }
+                    }
+                    // BOOK MODE
+                    FoldingFeature.Orientation.VERTICAL -> {
+//                        BoxWithConstraints (Modifier.fillMaxSize()) {
+//                            val hingeBounds = foldingFeature.bounds
+//                            val screenWidth = constraints.maxWidth
+//                        }
+
+                        if (extraContents.isEmpty()) {
+                            Box(modifier = modifier.fillMaxSize()) {
+                                content()
+                            }
+                        } else {
+                            Row(modifier = modifier.fillMaxSize()) {
+                                Box(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .fillMaxHeight()
+                                )
+                            }
+
+                            val allContents = listOf(content) + extraContents[0]
+                            Row(modifier = modifier.fillMaxSize()) {
+                                allContents.forEach {
+                                    Box(
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .fillMaxHeight()
+                                    ) {
+                                        it()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            // Opened up fully -> should show 1 full pane
+            FoldingFeature.State.FLAT -> {
+                // Show a single pane
+                println("Showing single pane")
+                Box(modifier = modifier.fillMaxSize()) {
+                    content()
+                }
+            }
+        }
     }
+
+
 }
